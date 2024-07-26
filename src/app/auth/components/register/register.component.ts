eb7df101f4fa74fe46ca4915a978cf06378de2ff
Maksym Vikolash <vikolashmaxim@gmail.com>
2024-07-26 11:28:39 +0300

    init commit

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+  }
+}
